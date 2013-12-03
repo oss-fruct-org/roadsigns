a66b956378d86e5c29563fa993fa48d6e5b670c0
Kirill Ivashov <ivashov@cs.karelia.ru>
2013-12-03 17:51:03 +0400

    Fix size of POI panel

--- src/org/fruct/oss/ikm/fragment/TestOverlay.java
+++ src/org/fruct/oss/ikm/fragment/TestOverlay.java
@@ -1,35 +1,20 @@
 package org.fruct.oss.ikm.fragment;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+import org.fruct.oss.ikm.R;
+import org.fruct.oss.ikm.Utils;
+import org.fruct.oss.ikm.service.Direction;
+import org.fruct.oss.ikm.service.Direction.RelativeDirection;
+import org.osmdroid.views.MapView;
+
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map.Entry;
 
-import org.fruct.oss.ikm.App;
-import org.fruct.oss.ikm.R;
-import org.fruct.oss.ikm.Utils;
-import org.fruct.oss.ikm.service.Direction;
-import org.fruct.oss.ikm.service.Direction.RelativeDirection;
-import org.fruct.oss.ikm.service.DirectionService;
-import org.osmdroid.views.MapView;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.support.v4.content.LocalBroadcastManager;
-import android.util.AttributeSet;
-import android.widget.RelativeLayout;
-
 public class TestOverlay extends RelativeLayout  {
-	public int panel_width = 0;
-	
-	private boolean isHidden = false;
-	
-	private MapView mapView;
-
-	private List<Direction> directions;
-	private float bearing;
 
 	private EnumMap<Direction.RelativeDirection, DirectionsPanel> panels = new EnumMap<Direction.RelativeDirection, DirectionsPanel>(
 			Direction.RelativeDirection.class);
@@ -39,20 +24,11 @@ public class TestOverlay extends RelativeLayout  {
 	}
 	
 	public void setHidden(boolean isHidden) {
-		this.isHidden = isHidden;
-		
 		for (DirectionsPanel panel : panels.values())
 			panel.setOverlayHidden(isHidden);
 	}
-
 	
 	public void initialize(MapView mapView) {
-		this.mapView = mapView;
-		
-	    panel_width = Utils.getDP(80);
-	    //Utils.log("asdasd " + panel_width);
-	    //Utils.log("asdasd  " + getContext().getResources().getDisplayMetrics().density);
-	    
 	    DirectionsPanel leftPanel = (DirectionsPanel) findViewById(R.id.directions_panel_left);
 	    DirectionsPanel rightPanel = (DirectionsPanel) findViewById(R.id.directions_panel_right);
 	    DirectionsPanel topPanel = (DirectionsPanel) findViewById(R.id.directions_panel_top);
@@ -92,15 +68,12 @@ public class TestOverlay extends RelativeLayout  {
 	
 	private void setTopPanelSize(DirectionsPanel panel) {
 		android.view.ViewGroup.LayoutParams params = panel.getLayoutParams();
-		params.width = getWidth() - Utils.getDP(80);
+		params.width = getWidth() - Utils.getDP(82);
 		
 		panel.setLayoutParams(params);
 	}
 	
 	public void setDirections(List<Direction> directions, final float bearing) {
-		this.directions = directions;
-		this.bearing = bearing;
-		
 		for (Entry<Direction.RelativeDirection, DirectionsPanel> entry : panels.entrySet()) {
 			final RelativeDirection relDir = entry.getKey();
 			final DirectionsPanel panel = entry.getValue();
@@ -109,7 +82,7 @@ public class TestOverlay extends RelativeLayout  {
 			Utils.select(directions, resultList, new Utils.Predicate<Direction>() {
 				public boolean apply(Direction dir) {
 					return dir.getRelativeDirection(bearing) == relDir;
-				};
+				}
 			});
 			
 			panel.setDirections(resultList);

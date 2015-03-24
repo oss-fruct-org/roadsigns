290d6fa80e341959272f7151443c8f23915a48b0
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-03-24 13:27:31 +0300

    Fix crash in help activity

--- RoadSigns/src/main/java/org/fruct/oss/ikm/HelpTabActivity.java
+++ RoadSigns/src/main/java/org/fruct/oss/ikm/HelpTabActivity.java
@@ -81,13 +81,8 @@ public class HelpTabActivity extends ActionBarActivity implements ActionBar.TabL
 		setupActionBar();
     }
 
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	private void setupActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		} else {
-			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		}
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
     @Override
